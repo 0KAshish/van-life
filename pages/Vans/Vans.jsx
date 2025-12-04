52dc94c6c9c365d2017125a6c60682113be89ca0
Ashish Kumar <Ashishsam.ceh1997@gmail.com>
2025-12-04 09:00:39 +0000

    Added type filter in vans page

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -13,7 +13,10 @@ export default function Vans() {
       .then((data) => setVans(data.vans));
   }, []);
 
-  const vanElements = vans.map((van) => (
+  const displayVans = typeFilter 
+  ? vans.filter(van=> van.type === typeFilter)
+  : vans;
+  const vanElements = displayVans.map((van) => (
     <div key={van.id} className="van-tile">
       <Link to={`/vans/${van.id}`}>
         <img src={van.imageUrl} />
@@ -32,6 +35,12 @@ export default function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <Link to="?type=simple" className="van-type simple">Simple</Link>
+        <Link to="?type=rugged" className="van-type rugged">Rugged</Link>
+        <Link to="?type=luxury" className="van-type luxury">Luxury</Link>
+        <Link to="." className="van-type clear-filters">Clear</Link>
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
