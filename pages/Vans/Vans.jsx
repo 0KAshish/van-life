9dbb1e77dded75c5a06c597972ece87298296265
Ashish Kumar <Ashishsam.ceh1997@gmail.com>
2025-12-05 09:50:50 +0000

    Added filter using useSearchParams

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -18,7 +18,8 @@ export default function Vans() {
   : vans;
   const vanElements = displayVans.map((van) => (
     <div key={van.id} className="van-tile">
-      <Link to={`/vans/${van.id}`}>
+      {/* <Link to={`/vans/${van.id}`}>            absolute path */}
+      <Link to={van.id}>                      { /* realtive path */}
         <img src={van.imageUrl} />
         <div className="van-info">
           <h3>{van.name}</h3>
@@ -35,13 +36,26 @@ export default function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
-      <div className="van-list-filter-buttons">
-        <Link to="?type=simple" className="van-type simple">Simple</Link>
-        <Link to="?type=rugged" className="van-type rugged">Rugged</Link>
-        <Link to="?type=luxury" className="van-type luxury">Luxury</Link>
-        <Link to="." className="van-type clear-filters">Clear</Link>
+      <div className="van-list-filter-buttons">        
+        <button className={`van-type simple ${typeFilter==="simple"? "selected": null}`} onClick={()=>setSearchParams({type: "simple"})}>Simple</button>
+        <button className={`van-type luxury ${typeFilter==="luxury"? "selected": null}`} onClick={()=>setSearchParams({type: "luxury"})}>Luxury</button>
+        <button className={`van-type rugged ${typeFilter==="rugged"? "selected": null}`} onClick={()=>setSearchParams({type: "rugged"})}>Rugged</button>
+        {typeFilter && <button className="van-type clear-filters" onClick={()=>setSearchParams({})}>Clear filter</button>}
       </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
 }
+
+        // Other Ways to add setsearch pararms are below
+
+        // <Link to="?type=simple" className="van-type simple">Simple</Link>
+        // <Link to="?type=rugged" className="van-type rugged">Rugged</Link>
+        // <Link to="?type=luxury" className="van-type luxury">Luxury</Link>
+        // <Link to="." className="van-type clear-filters">Clear</Link>
+
+        
+        // <button className="van-type simple" onClick={()=>setSearchParams("?type=simple")}>Simple</button>
+        // <button className="van-type luxury" onClick={()=>setSearchParams("?type=luxury")}>Luxury</button>
+        // <button className="van-type rugged" onClick={()=>setSearchParams("?type=rugged")}>Rugged</button>
+        // <button className="van-type clear-filters" onClick={()=>setSearchParams(".")}>Clear filter</button>
